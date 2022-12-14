d488cd107d8f7bf352100ae99a46cd97cc300354
Sajjad Toomari <toomaris@gmail.com>
2022-12-14 19:33:59 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Playwright;
+using PlaywrightDemo;
+using Serilog;
+using System.Text.Json;
+
+//Initilize console logger
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+//Read config from json
+var configPath = Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), "config.json");
+StreamReader streamReader = new (configPath);
+var settings = await JsonSerializer.DeserializeAsync<Settings>(streamReader.BaseStream);
+
+//Playwright initializer
+using var playwright = await Playwright.CreateAsync();
+
+Log.Information("Playwright initilized");
+
+//Launch chromium browser and save user data (cookies, etc)
+await using var browser = await playwright.Chromium.LaunchPersistentContextAsync("data", new BrowserTypeLaunchPersistentContextOptions
+{
+    Headless = settings.Headless,
+});
+
+Log.Information("Chromium launched");
+
+var page = await browser.NewPageAsync();
+
+await Login(page);
+await GoToUploadPage(page);
+await UploadVideos(page);
+
+async Task Login(IPage page)
+{
+    await page.GotoAsync(settings.CmsLoginUrl);
+    if (page.Url.Contains("accounts/login"))
+    {
+        //Fill username input
+        await page.Locator(settings.LoginInputUsernameSelector).FillAsync(settings.CmsAdminUser);
+
+        //Wait one second
+        await Task.Delay(1000);
+
+        //Fill password input
+        await page.Locator(settings.LoginInputPasswordSelector).FillAsync(settings.CmsAdminPassword);
+
+        //Wait one second
+        await Task.Delay(1000);
+
+        //Click login button
+        await page.Locator(settings.LoginButtonSelector).ClickAsync();
+
+        Log.Information("Logged in");
+    }
+    else
+    {
+        Log.Information("No need to login in");
+    }
+}
+
+async Task GoToUploadPage(IPage page)
+{
+    //Go to upload page
+    await page.GotoAsync(settings.CmsUploadUrl);
+
+    //Wait one second
+    await Task.Delay(1000);
+
+    Log.Information("Opened upload page");
+}
+
+async Task UploadVideos(IPage page)
+{
+    //Get videos path
+    var videosPath = Directory.GetFiles(settings.UploadVideosDirectory, "*.mp4", SearchOption.TopDirectoryOnly);
+
+    foreach (var videoPath in videosPath)
+    {
+        await page.SetInputFilesAsync(settings.UploadButtonSelector, videoPath);
+
+
+
+        await Task.Delay(2000);
+    }
+
+}
+
+await Task.Delay(int.MaxValue);
---
+++ Settings.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PlaywrightDemo;
+
+public class Settings
+{
+    [JsonPropertyName("headless")]
+    public bool Headless { get; set; }
+
+    [JsonPropertyName("cms_admin_user")]
+    public string CmsAdminUser { get; set; }
+
+    [JsonPropertyName("cms_admin_password")]
+    public string CmsAdminPassword { get; set; }
+
+    [JsonPropertyName("cms_login_url")]
+    public string CmsLoginUrl { get; set; }
+
+    [JsonPropertyName("cms_upload_url")]
+    public string CmsUploadUrl { get; set; }
+
+    [JsonPropertyName("login_input_username_selector")]
+    public string LoginInputUsernameSelector { get; set; }
+
+    [JsonPropertyName("login_input_password_selector")]
+    public string LoginInputPasswordSelector { get; set; }
+
+    [JsonPropertyName("login_button_selector")]
+    public string LoginButtonSelector { get; set; }
+
+    [JsonPropertyName("upload_button_selector")]
+    public string UploadButtonSelector { get; set; }
+
+    [JsonPropertyName("upload_videos_directory")]
+    public string UploadVideosDirectory { get; set; }
+}
+
